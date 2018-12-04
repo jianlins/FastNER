3d5b8688d46b8f69123663d745c2e56ed766f38c
jianlins <jianlinshi.cn@gmail.com>
2018-12-03 19:53:52 -0700

    add test resources

---
+++ src/test/java/edu/utah/bmi/nlp/core/NERSpanTest.java
@@ -0,0 +1,16 @@
+package edu.utah.bmi.nlp.core;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NERSpanTest {
+    @Test
+    public void test() {
+        NERSpan a = new NERSpan(1, 3, 0, 2, 0.5, "ab");
+        NERSpan b = new NERSpan(1, 4, 1, 1, 0.5, "ab");
+        System.out.println(a.compareTo(b));
+
+    }
+
+}
